980e2bd5c7ddb499c344b8bdf6fb052ac6210b13
oqipo Dev <100394455+oqipoDev@users.noreply.github.com>
2022-12-30 12:35:42 -0600

    Create basicUI.js

---
+++ js/basicUI.js
@@ -0,0 +1,32 @@
+function RestoreSession(){
+    RestoreTheme();
+}
+
+function RestoreTheme(){
+    let dark = (sessionStorage.getItem("darkTheme"));
+
+    if (dark == undefined){
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches){
+            document.body.classList.add("dark");
+        }
+    }
+    
+    if (dark == 1){
+        document.body.classList.add("dark");
+    }
+}
+
+function SwitchTheme(){
+    let dark = (sessionStorage.getItem("darkTheme"));
+
+    if (dark == undefined) dark = 0;
+
+    if (dark == 1){
+        document.body.classList.remove("dark");
+        sessionStorage.setItem("darkTheme", 0);
+    }
+    else {
+        document.body.classList.add("dark");
+        sessionStorage.setItem("darkTheme", 1);
+    }
+}
